aad1b236518bc490fc59fe01a3fd27ab7e17617a
kota nishikawa <koonii26@gmail.com>
2023-08-31 00:26:03 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import './App.css';
+
+function App() {
+  // 仮の画像データ
+  const images = [
+    '/images/1poto.jpg',
+    '/images/2poto.jpg',
+    '/images/3poto.jpg',
+    '/images/sam.jpg',
+    '/images/sam2.jpg',
+    // 他の画像...
+  ];
+
+  return (
+    <Grid container spacing={3} justify="center" alignItems="center">
+
+      {images.map((image, index) => (
+        <Grid item xs={12} sm={6} md={6} key={index}>
+          <img src={image} alt={`portfolio ${index + 1}`} className="responsive-image" />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+export default App;
